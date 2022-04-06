256c252b36186a8511ab157cf2f8bee62a90f903
Paras Dalsaniya <paras.dalsaniya@inaraconsultancy.com>
2022-04-06 13:13:02 +0530

    add room

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const index = require("./routes/index");
-const cors = require("cors")
+const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(cors())
@@ -16,19 +17,51 @@ const io = socketIo(server, {
   }
 });
 
-
 io.on("connection", (socket) => {
 
+  socket.on("join_room", (data) => {
+    console.log('data of room', data)
+    socket.join(data);
+  });
+
   socket.on("send_message", (data) => {
+    console.log("data of send msg", data);
     // socket.broadcast.emit("receive_message", data)
     socket.to(data.room).emit("receive_message", data)
+  })
+
+  socket.on("set_orerId", (data) => {
+    console.log("data of set_orerId", data);
+    socket.join(data)
+  })
+
+  socket.on("send_status", (data) => {
+    console.log('data of send_status', data)
+    axios
+      .get(
+        `https://sandbox.scoopm.com/v1/dispatch/tasks/-Mzx_aOajAuxjn55C8Nd/status`,
+        {
+          auth: {
+            username: "scoopmeals",
+            password: "LHB2oA3nOYmZ1bFYEhe4dSyjF9FLTRiX",
+          },
+        }
+      )
+      .then((resp) => {
+        console.log('resp.data', resp.data)
+        socket.to(data.orderId).emit("receive_status", resp.data)
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+    
   })
 
 
 })
   
 
-let interval;
+// let interval;
 
 // io.on("connection", (socket) => {
 //   console.log("New client connected");
@@ -42,13 +75,13 @@ let interval;
 //   });
 // });
 
-const getApiAndEmit = socket => {
-    const response = new Date();
-    socket.emit("FromAPI", response);
-};
+// const getApiAndEmit = socket => {
+//     const response = new Date();
+//     socket.emit("FromAPI", response);
+// };
 
-server.listen(8000, () => {
-    console.log("App run on 8000");
+server.listen(8001, () => {
+    console.log("App run on 8001");
 })
 
 // #####################################
